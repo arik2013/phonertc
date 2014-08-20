610c665b7d1ce06e01d53f8f63ac12753465979d
Joseph Frazier <joseph@onsip.com>
2014-08-19 22:59:26 -0400

    PhoneRTCPlugin.java: set up Actions enum for execute method. TODO impls

--- src/android/com/dooble/phonertc/PhoneRTCPlugin.java
+++ src/android/com/dooble/phonertc/PhoneRTCPlugin.java
@@ -1,8 +1,11 @@
 package com.dooble.phonertc;
 
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.webrtc.PeerConnectionFactory;
 
 public class PhoneRTCPlugin extends CordovaPlugin {
@@ -13,5 +16,59 @@ public class PhoneRTCPlugin extends CordovaPlugin {
     super.initialize(cordova, webView);
 
     factory = new PeerConnectionFactory();
+  }
+
+  @Override
+  public boolean execute (String action, JSONArray args,
+      CallbackContext callbacks) throws JSONException {
+    Actions.valueOf(action.toUpperCase()).execute(args, callbacks);
+    return true;
+  }
+
+  // see http://stackoverflow.com/a/2667671
+  private enum Actions {
+    CONSTRUCT {
+      @Override
+      public void execute (JSONArray args, CallbackContext callbacks) {
+        // TODO
+      }
+    },
+
+    ISREADY {
+      @Override
+      public void execute (JSONArray args, CallbackContext callbacks) {
+        // TODO
+      }
+    },
+
+    GETDESCRIPTION {
+      @Override
+      public void execute (JSONArray args, CallbackContext callbacks) {
+        // TODO
+      }
+    },
+
+    SETDESCRIPTION {
+      @Override
+      public void execute (JSONArray args, CallbackContext callbacks) {
+        // TODO
+      }
+    },
+
+    RENDER {
+      @Override
+      public void execute (JSONArray args, CallbackContext callbacks) {
+        // TODO
+      }
+    },
+
+    CLOSE {
+      @Override
+      public void execute (JSONArray args, CallbackContext callbacks) {
+        // TODO
+      }
+    };
+
+    public abstract void execute (JSONArray args, CallbackContext callbacks);
   }
 }
